ddad24a2975caa2c92b77e7b06a1cca5c30a516d
Guilherme Amaral <guilherme-amaral-dias@hotmail.com>
2024-10-30 09:47:23 -0300

    first commit

---
+++ src/main/java/com/mockstudy/mockitostudy/MockitostudyApplication.java
@@ -0,0 +1,13 @@
+package com.mockstudy.mockitostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitostudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitostudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockstudy/mockitostudy/MockitostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockstudy.mockitostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitostudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
